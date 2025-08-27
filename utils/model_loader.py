8c01df02bd14a3a88703818b57789030b3a4bebd
dgsuma <dumindags@gmail.com>
2025-08-27 19:30:46 +1000

    updated model_loader.py file

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -1,16 +1,11 @@
 import os
 import sys
+import json
 from dotenv import load_dotenv
 from utils.config_loader import load_config
-
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
-from langchain_community.chat_models import ChatOllama
-from langchain_together import ChatTogether
-
-# from langchain_openai import ChatOpenAI
-
-from logger.custom_logger import CustomLogger
+from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import DocumentPortalException
 
 log = CustomLogger().get_logger(__name__)
