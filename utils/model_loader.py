605fcc9fcd21e64e13162d2f33eee742f3d4acad
dgsuma <dumindags@gmail.com>
2025-08-28 20:03:27 +1000

    updated model_loader.py file

--- utils/model_loader.py
+++ utils/model_loader.py
@@ -8,8 +8,6 @@ from langchain_groq import ChatGroq
 from logger import GLOBAL_LOGGER as log
 from exception.custom_exception import DocumentPortalException
 
-#log = CustomLogger().get_logger(__name__)
-
 class ApiKeyManager:
     pass
     REQUIRED_KEYS = ["GROQ_API_KEY", "GOOGLE_API_KEY"]
